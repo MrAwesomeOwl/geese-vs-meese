2a284e3ec705b04db6cd16e4b1cd365620bc46e1
MrAwesomeOwl <140653655+MrAwesomeOwl@users.noreply.github.com>
2024-09-03 10:52:52 -0400

    moose now damages player

--- scripts/moose.gd
+++ scripts/moose.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 ## how fast the moose should move
-@export var SPEED = 100.0;
+@export var SPEED = 40.0;
 
 ## how much damage to deal to the player when hitting them
 @export var DAMAGE = 1;
@@ -18,7 +18,6 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 		
 	# if moose is touching a wall, flip its move direction
-	print(is_on_wall())
 	if is_on_wall(): 
 		direction *= -1
 		
@@ -26,11 +25,11 @@ func _physics_process(delta: float) -> void:
 	velocity.x = direction * SPEED
 	
 	# make moose face the correct direction
-	$Visuals.scale.x = direction
+	$Body.scale.x = direction
 	
 	move_and_slide()
 
 # runs whenever the damage hitbox touches something
 func _on_damage_hitbox_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		body.get_node("Health").current -= 1
+		body.get_node("Health").damage(DAMAGE)
--- scripts/nodes/healthStat.gd
+++ scripts/nodes/healthStat.gd
@@ -5,13 +5,27 @@ class_name HealthStat
 signal on_current_changed(new_health: float, old_health: float)
 signal on_max_changed(new_max_health: float, old_max_health: float)
 
+## maximum health
 @export var maximum = 3.0:
 	set(new):
 		maximum = new
 		current = min(current,maximum)
 		on_max_changed.emit(new,max)
 		
+## current health
 @export var current = 3.0:
 	set(new):	
 		current = new
 		on_current_changed.emit(new,current)
+
+## how many seconds to be invulnerable for after taking damage
+@export var invulnerability_time = 0.0
+
+## unit: msec
+var last_damaged_at = -INF
+
+func damage(amount: float):
+	# if it has been long enough since the last damage that you are no longer invulnerable
+	if Time.get_ticks_msec() - last_damaged_at > invulnerability_time:
+		current = clampf(current - amount,0,INF)
+		last_damaged_at = Time.get_ticks_msec()
