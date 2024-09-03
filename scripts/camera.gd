719369c123923471bfb23d225ed64cdc564fe272
MrAwesomeOwl <140653655+MrAwesomeOwl@users.noreply.github.com>
2024-09-03 19:01:21 -0400

    better camera

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -4,4 +4,4 @@ extends Camera2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	position = player.position
+	position = Util.smooth_step(position,player.position - Vector2(0,10),0.7,delta)
