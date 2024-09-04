1700aebf0a35d767c08e53dd8eccba7b088dc69d
MrAwesomeOwl <140653655+MrAwesomeOwl@users.noreply.github.com>
2024-09-04 17:46:47 -0400

    goose can now fulfill its violent fantasies
    good for him
    
    or her
    (dontr cancel me)

--- scripts/moose.gd
+++ scripts/moose.gd
@@ -15,25 +15,55 @@ extends CharacterBody2D
 
 @onready var direction = START_DIRECTION	
 
+var last_frame_velocity: Vector2
+
+func take_knockback(kb_velocity: Vector2, seconds: float):
+	velocity = kb_velocity
+	
+
 func _physics_process(delta: float) -> void:
+	if $Health.is_dead:
+		modulate.a -= delta*1.5
+		modulate.r += delta/2
+		$Body/Animations.rotation_degrees += delta*20
+	else:
+			
+		# if moose is touching a wall, flip its move direction
+		if is_on_wall(): 
+			direction = get_wall_normal().x
+			velocity.x = last_frame_velocity.x * -1
+			
+		# actual movement
+		velocity.x = move_toward(velocity.x,direction * SPEED,SPEED * 10 * delta)
+		
+		# make moose face the correct direction
+		$Body.scale.x = direction
+		
 	# gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		
-	# if moose is touching a wall, flip its move direction
-	if is_on_wall(): 
-		direction *= -1
-		
-	# actual movement
-	velocity.x = direction * SPEED
-	
-	# make moose face the correct direction
-	$Body.scale.x = direction
-	
+	last_frame_velocity = velocity
 	move_and_slide()
 
 # runs whenever the damage hitbox touches something
 func _on_damage_hitbox_body_entered(body: Node2D) -> void:
-	if body.is_in_group("player"):
+	if body.is_in_group("player") and not $Health.is_dead:
 		body.get_node("Health").damage(DAMAGE)
-		body.knockback(Vector2(sign(body.global_position.x - global_position.x),-0.5) * KNOCKBACK,0.1)
+		body.take_knockback(Vector2(sign(body.global_position.x - global_position.x),-0.5) * KNOCKBACK,0.1)
+
+
+func _on_death() -> void:
+	# let moose fall through the floor
+	$CollisionShape2D.disabled = true
+
+	# flip upside-down
+	$Body/Animations.rotation_degrees += 180
+	$Body/Animations.scale.x *= -1
+	
+	# launch up
+	velocity.y = -200
+	
+	# despawn once completely faded out
+	await Util.wait(1)
+	queue_free()
--- scripts/player.gd
+++ scripts/player.gd
@@ -2,10 +2,16 @@ extends CharacterBody2D
 
 
 @export var SPEED = 150.0
+## lower values result in slipperier movement (like ice)
 @export_range(0,10) var SNAPPINESS = 5
-@export var JUMP_VELOCITY = -300.0
+@export var JUMP_STRENGTH = 300.0
+@export var DAMAGE = 1.0
+@export var KNOCKBACK = 300.0
 
+@export_group("Internal stuff (ignore)")
 var facing_direction = 1.0
+## if true, facing_direction won't automatically change
+@export var facing_direction_locked: bool
 
 var knockback_vector: Vector2 = Vector2(0,0)
 var time_knockback_expire: int = 0 #msec
@@ -13,7 +19,7 @@ var time_knockback_expire: int = 0 #msec
 @onready var animation_tree: AnimationTree = $AnimationTree
 
 ## knocks back the player
-func knockback(velocity: Vector2,seconds: float):
+func take_knockback(velocity: Vector2,seconds: float):
 	# apply knockback
 	knockback_vector = velocity
 	
@@ -34,6 +40,20 @@ func damage_flash_loop():
 		await Util.wait(0.1)
 		modulate = Color(1,1,1,1)
 		await Util.wait(0.1)
+	
+## main attack code	
+func attack():
+	# for everything the damage hitbox is touching
+	for hit_body in $Body/DamageHitbox.get_overlapping_bodies():
+		var hit_health: HealthStat = hit_body.get_node("Health")
+			
+		# if the thing has health, damage it
+		if hit_health:
+			hit_health.damage(1)
+		
+		# if the thing can take knockback, do that
+		if hit_body.has_method("take_knockback") and not (hit_health and hit_health.is_dead):
+			hit_body.take_knockback(Vector2(sign(hit_body.global_position.x - global_position.x),-0.5) * KNOCKBACK,0.2)
 
 func _ready():
 	damage_flash_loop()
@@ -45,12 +65,13 @@ func _physics_process(delta: float) -> void:
 
 	# jumping
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = -JUMP_STRENGTH
 
 	# left/right movement
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		facing_direction = direction
+		if not facing_direction_locked:
+			facing_direction = direction
 		animation_tree["parameters/movement/playback"].travel("run")
 		
 		velocity.x = move_toward(velocity.x, SPEED * direction, SPEED*SNAPPINESS * delta)
